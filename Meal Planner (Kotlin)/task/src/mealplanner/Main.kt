4c0a44d9ad2b1d0838b57f9b165fc938e7a5f4e6
Omar Osman <zkotliner@gmail.com>
2024-08-06 18:14:41 +0900

    Complete Stage 2 - Create menu

--- Meal Planner (Kotlin)/task/src/mealplanner/Main.kt
+++ Meal Planner (Kotlin)/task/src/mealplanner/Main.kt
@@ -1,30 +1,99 @@
 package mealplanner
 
-data class Meal(val category: String, val name: String, val ingredients: Set<String>)
+data class Meal(val category: String, val name: String, val ingredients: Set<String>) {
+    override fun toString(): String {
+        return buildString {
+            appendLine("Category: $category")
+            appendLine("Name: $name")
+            appendLine("Ingredients:")
+            ingredients.forEach(::appendLine)
+        }
+    }
+}
+
+enum class MealCategory(val value: String) {
+    BREAKFAST("breakfast"),
+    LUNCH("lunch"),
+    DINNER("dinner")
+}
+
+private val dailyMeals = mutableListOf<Meal>()
+
+// Validation Regex
+val lettersOnlyRegex = "^[A-Za-z ]+$".toRegex()
 
 fun main() {
+    while (true) {
+        println("What would you like to do (add, show, exit)?")
+        when (readln()) {
+            "add" -> addMeal()
+            "show" -> showMeal()
+            "exit" -> {
+                println("Bye!")
+                break
+            }
+
+            else -> continue
+        }
+    }
+}
+
+private fun showMeal() {
+    println()
+    with(dailyMeals) {
+        forEach(::println)
+        ifEmpty { println("No meals saved. Add a meal first.") }
+    }
+}
+
+private fun addMeal() {
     // Ask about the meal category
     println("Which meal do you want to add (breakfast, lunch, dinner)?")
 
-    // Accept user category
-    val mealCategory = readln()
+    // Accept user meal category
+    val mealCategory = readMealCategory()
 
-    // Accept user meal name
+    // Accept user meal name + validate
     println("Input the meal's name:")
-    val mealName = readln()
+    val mealName = readMealName()
 
     // Accept ingredients list
     println("Input the ingredients:")
-    val ingredientsList = readln().split(",").map { it.trim() }.toSet()
+    val ingredientsList: MutableSet<String> = readIngredientList()
 
     // Create the meal with input info
     val meal = Meal(mealCategory, mealName, ingredientsList)
 
-    // Output Meal info.
-    println("Category: ${meal.category}")
-    println("Name: ${meal.name}")
-    println("Ingredients:")
-    meal.ingredients.forEach { ingredient -> println(ingredient) }
-    println("The meal has been added!")
+    //Add meal to Meal-list
+    dailyMeals.add(meal)
 
+    println("The meal has been added!")
+}
+
+private fun readMealCategory(): String {
+    val mealCategory = readln()
+    while (MealCategory.values().none { it.value == mealCategory }) {
+        println("Wrong meal category! Choose from: breakfast, lunch, dinner.")
+        return readMealCategory()
+    }
+    return mealCategory
+}
+
+private fun readMealName(): String {
+    val mealName = readln()
+    while (!mealName.matches(lettersOnlyRegex)) {
+        println("Wrong format. Use letters only!")
+        return readMealName()
+    }
+    return mealName
+}
+
+private fun readIngredientList(): MutableSet<String> {
+    val ingredientsList: MutableSet<String> = readln().split(",").map { it.trim() }.toMutableSet()
+    while (true) {
+        if (ingredientsList.all { it.matches(lettersOnlyRegex) }) break
+        println("Wrong format. Use letters only!")
+        return readIngredientList()
+    }
+    return ingredientsList
 }
